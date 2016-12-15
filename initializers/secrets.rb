76abad97a30f799affbed2612ac6567c37b89277
Sahil Prajapati <sahilprjpt206@gmail.com>
2016-12-15 15:34:44 +0530

    Create secrets.rb

---
+++ initializers/secrets.rb
@@ -0,0 +1,4 @@
+ENV["CONSUMER_KEY"] = "xxxkJhDxxxxxxxxxxxxxxxzxx0"
+ENV["CONSUMER_SECRET"] = "0xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXV"
+ENV["ACCESS_TOKEN"] = "xxxxxxxxxxxxxxxxxxxxxx"
+ENV["ACCESS_TOKEN_SECRET"] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
